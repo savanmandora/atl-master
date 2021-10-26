f7906874920c3ff8f746eb3d518f3c1c9b99a73c
Savan Mandora <86867589+savanmandora@users.noreply.github.com>
2021-10-26 08:29:39 +0530

    Add files via upload
    And GATE by the help of two switches and arduino.

---
+++ Arduino Projects/AND_GATE_by_two_switches/AND_GATE_by_two_switches.ino
@@ -0,0 +1,31 @@
+const int sw1=6;
+const int sw2=7;
+const int led=13;
+int sw1_state=0;
+int sw2_state=0;
+
+
+void setup()
+{
+  pinMode(6,INPUT);
+  pinMode(7,INPUT);
+  pinMode(13,OUTPUT);
+  
+  }
+
+void loop()
+{
+  sw1_state=digitalRead(sw1);
+  sw2_state=digitalRead(sw2);
+
+  if(sw1_state&&sw2_state==1)
+  {
+    digitalWrite(13,HIGH);
+    
+    }
+  else{
+    digitalWrite(13,LOW);
+    }  
+    
+  
+  }
